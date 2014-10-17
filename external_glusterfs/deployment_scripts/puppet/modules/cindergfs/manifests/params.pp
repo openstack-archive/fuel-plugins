1fa059addce88e8fbdbf6ba037383def4fd91fbc
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2014-10-17 16:43:06 +0400

    Added GlusterFS puppet plugin
    Implements: blueprint cinder-neutron-plugins-in-fuel
    Change-Id: I1d92f27eca6b006192b1d8553f997a59e37ce014

---
+++ external_glusterfs/deployment_scripts/puppet/modules/cindergfs/manifests/params.pp
@@ -0,0 +1,19 @@
+#
+class cindergfs::params {
+
+  $cinder_conf = '/etc/cinder/cinder.conf'
+
+  if $::osfamily == 'Debian' {
+    $glusterfs_package_name = 'glusterfs-client'
+    $package_name           = 'cinder-volume'
+    $volume_service_name    = 'cinder-volume'
+
+  } elsif($::osfamily == 'RedHat') {
+    $glusterfs_package_name = 'glusterfs'
+    $package_name           = 'openstack-cinder'
+    $volume_service_name    = 'openstack-cinder-volume'
+
+  } else {
+    fail("unsuported osfamily ${::osfamily}, currently Debian and Redhat are the only supported platforms")
+  }
+}
