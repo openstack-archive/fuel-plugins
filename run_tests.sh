2a4985611d15509474a680a0d4b72af2288f4c9c
Evgeniy L <eli@mirantis.com>
2014-10-22 16:23:03 +0400

    Add fuel plugin example
    The plugin shows how user can create plugin
    with packages, and deploy the packages
    
    Change-Id: I6bb272024bae920b48f936347829692503e8b07b
    Implements: blueprint cinder-neutron-plugins-in-fuel

--- run_tests.sh
+++ run_tests.sh
@@ -49,6 +49,8 @@ function process_options {
 
 # Settings
 ROOT=$(dirname `readlink -f $0`)
+FPB_VENV_PATH=${FPB_VENV_PATH:-"${ROOT}/fpb_venv"}
+BUILT_PLUGINS_PATH=${BUILT_PLUGINS_PATH:-"${ROOT}/built_plugins"}
 
 # Initialize global variables
 plugins_build=0
@@ -112,8 +114,33 @@ function run_fpb {
 
 # Build all of the plugins with current version of plugins builder
 function run_build {
-  # TODO(eli): add plugins building process plugin
-  # when there are any plugins
+  virtualenv $FPB_VENV_PATH
+
+  # virtualenv has unbound variables
+  # disable it during virtualenv usage
+  set +u
+
+  source $FPB_VENV_PATH/bin/activate || return 1
+  fpb_path=$ROOT/fuel_plugin_builder/
+  pushd $fpb_path
+  python setup.py install
+  popd
+
+  mkdir -p $BUILT_PLUGINS_PATH
+  rm $BUILT_PLUGINS_PATH/*.fp
+
+  # Find plugins
+  for dir in $ROOT/*/metadata.yaml;
+  do
+    plugin_dir=$(dirname $dir);
+    pushd $plugin_dir
+    fpb --build $plugin_dir --debug || return 1
+    cp *.fp $BUILT_PLUGINS_PATH/
+    popd
+  done
+
+  deactivate
+  set -u
   return 0
 }
 
