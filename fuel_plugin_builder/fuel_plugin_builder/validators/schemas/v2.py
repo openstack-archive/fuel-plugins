a87094bb20583fcf3f64062a31f08c8a15fb8d21
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2015-01-27 11:38:41 +0300

    Reboot task type for plugins
    Add support for new task type 'reboot'.
    It is apply only one parameters 'timeout' which mean -
    how much time we should wait after sending reboot command
    to detect fact of node restarting.
    
    This change require to add new validation schema (v2).
    
    TODO:
    
    - tests.
    
    Implements blueprint reboot-action-for-plugin
    
    Change-Id: I86be0f8baf72ef245ab489ddb9e7e1b1f9d2b605

---
+++ fuel_plugin_builder/fuel_plugin_builder/validators/schemas/v2.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from base import BaseSchema
+
+
+class SchemaV2(BaseSchema):
+
+    @classmethod
+    def metadata_schema(self):
+        basic_schema = super(SchemaV2, self).metadata_schema()
+        basic_schema['properties']['package_version'] = {
+            'enum': ['2.0.0']
+        }
+        return basic_schema
+
+    @classmethod
+    def task_schema(self):
+        basic_schema = super(SchemaV2, self).task_schema()
+        basic_schema['properties']['type'] = {
+            'enum': ['puppet', 'shell', 'reboot']}
+        return basic_schema
+
+    @classmethod
+    def reboot_parameters(self):
+        return {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'object',
+            'required': ['timeout'],
+            'properties': {
+                'timeout': self.positive_integer()}
+        }
