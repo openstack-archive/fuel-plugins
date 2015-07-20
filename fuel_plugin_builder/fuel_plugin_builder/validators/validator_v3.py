3f34b24fff9b6835f23b3e4bb3ced9a92ed4c39d
Alexander Saprykin <asaprykin@mirantis.com>
2015-07-20 13:16:27 +0300

    Add network_roles.yaml to plugin templates V3
    Change-Id: I4a16a1490a931d8f192008fcc0c13472bcb8a5a6
    Implements: blueprint vip-reservation

--- fuel_plugin_builder/fuel_plugin_builder/validators/validator_v3.py
+++ fuel_plugin_builder/fuel_plugin_builder/validators/validator_v3.py
@@ -14,6 +14,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from os.path import join as join_path
+
+from fuel_plugin_builder import utils
 from fuel_plugin_builder.validators.schemas import v3
 from fuel_plugin_builder.validators import validator_v2
 
@@ -22,6 +25,21 @@ class ValidatorV3(validator_v2.ValidatorV2):
 
     schema = v3.SchemaV3()
 
+    def __init__(self, *args, **kwargs):
+        super(ValidatorV3, self).__init__(*args, **kwargs)
+        self.network_roles_path = join_path(
+            self.plugin_path, 'network_roles.yaml')
+
     @property
     def basic_version(self):
         return '7.0'
+
+    def validate(self):
+        super(ValidatorV3, self).validate()
+        self.check_network_roles()
+
+    def check_network_roles(self):
+        network_roles = utils.parse_yaml(self.network_roles_path)
+        self.validate_schema(
+            network_roles, self.schema.network_roles_schema,
+            self.network_roles_path)
