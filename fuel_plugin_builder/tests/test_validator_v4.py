b8e33ebbd9132bcc79f3e5e46aa8d36f8ba1654a
ekosareva <ekosareva@mirantis.com>
2015-10-26 05:55:28 +0400

    Validation for components metadata in FPB
    * Adding validation of components.yaml metadata file for V4 plugins
    * Adding components.yaml file for fuel_plugin_example_v4
    
    Change-Id: I7b65371c5330abbb0b724e0b6faca66d033d9898
    Implements: blueprint component-registry

--- fuel_plugin_builder/tests/test_validator_v4.py
+++ fuel_plugin_builder/tests/test_validator_v4.py
@@ -16,38 +16,29 @@
 
 import mock
 
-from fuel_plugin_builder.tests.base import BaseValidator
+from fuel_plugin_builder import errors
+from fuel_plugin_builder.tests.test_validator_v3 import TestValidatorV3
 from fuel_plugin_builder.validators.schemas import SchemaV4
 from fuel_plugin_builder.validators.validator_v4 import ValidatorV4
 
 
-class TestValidatorV4(BaseValidator):
+class TestValidatorV4(TestValidatorV3):
 
     __test__ = True
     validator_class = ValidatorV4
     schema_class = SchemaV4
 
-    def test_validate(self):
-        mocked_methods = ['check_deployment_tasks']
-        super(TestValidatorV4, self).test_validate(
-            additional_mocked_methods=mocked_methods)
-
-        self.validator.check_deployment_tasks.assert_called_once_with()
-
-    # copy-pasted from tests v3 to
-    # - override invalid base validator
-    # - make additional validator class integrity smoke-test
-
     def test_check_schemas(self):
         mocked_methods = [
             'check_env_config_attrs',
-            'validate_file_by_schema',
             'check_deployment_tasks_schema',
             'check_network_roles_schema',
             'check_node_roles_schema',
-            'check_volumes_schema'
+            'check_volumes_schema',
+            'check_components_schema'
         ]
         self.mock_methods(self.validator, mocked_methods)
+        self.mock_methods(self.validator, ['validate_file_by_schema'])
         self.validator.check_schemas()
 
         self.assertEqual(
@@ -57,11 +48,8 @@ class TestValidatorV4(BaseValidator):
                        self.validator.tasks_path, check_file_exists=False)],
             self.validator.validate_file_by_schema.call_args_list)
 
-        self.validator.check_env_config_attrs.assert_called_once_with()
-        self.validator.check_deployment_tasks_schema.assert_called_once_with()
-        self.validator.check_network_roles_schema.assert_called_once_with()
-        self.validator.check_node_roles_schema.assert_called_once_with()
-        self.validator.check_volumes_schema.assert_called_once_with()
+        for method in mocked_methods:
+            getattr(self.validator, method).assert_called_once_with()
 
     @mock.patch('fuel_plugin_builder.validators.base.utils')
     def test_check_compatibility_failed(self, utils_mock):
@@ -91,3 +79,124 @@ class TestValidatorV4(BaseValidator):
             'fuel_version': ['8.0'],
             'package_version': '4.0.0'}
         self.validator.check_compatibility()
+
+    def test_check_components_schema_validation_failed(self):
+        data_sets = [
+            {
+                'name': 'test_additional_item',
+                'type': 'network',
+                'label': 'test label',
+                'compatible': []
+            },
+            {
+                'name': 'test_wrong_label_type',
+                'label': 1
+            },
+            {
+                'name': 'test_wrong_description_type',
+                'description': []
+            },
+            {
+                'compatible': [],
+                'incompatible': []
+            },
+            {
+                'name': 'wrong::type_name:*',
+                'compatible': [],
+                'incompatible': []
+            },
+            {
+                'name': 'storage::NameWithUpperCase',
+                'label': 'Component Label'
+            },
+            {
+                'name': 'storage::wrong_compatible_types',
+                'compatible': {},
+                'requires': 3,
+                'incompatible': ""
+            },
+            {
+                'name': 'storage:no_name_compatible_items',
+                'incompatible': [{
+                    'message': 'Component incompatible with XXX'
+                }],
+            },
+            {
+                'name': 'storage:wrong_message_compatible_items',
+                'incompatible': [{
+                    'name': 'storage:*',
+                    'message': 1234
+                }]
+            },
+            {
+                'name': 'network:new_net:wrong_compatible',
+                'compatible': [
+                    {'name': ''},
+                    {'name': 'wrong::component'},
+                    {'name': 'storage:UpperCaseWrongName'},
+                    {'name': 'Another_wrong**'}
+                ]
+            }
+        ]
+
+        with mock.patch('fuel_plugin_builder.validators.base.utils') as \
+                mock_utils:
+            for data in data_sets:
+                mock_utils.parse_yaml.return_value = [data]
+                self.assertRaises(errors.ValidationError,
+                                  self.validator.check_components_schema)
+
+    def test_check_components_schema_validation_passed(self):
+        data_sets = [
+            {
+                'name': 'network:test_name',
+                'label': 'Test Name network'
+            },
+            {
+                'name': 'storage:sub-type:test_name',
+                'label': 'Test Storage',
+                'description': 'New Test Storage Description',
+                'compatible': [
+                    {'name': 'hypervisor:libvirt:*'},
+                    {'name': 'hypervisor:wmvare_new_1'},
+                    {'name': 'network:neutron:ml2:*'},
+                    {'name': 'additional_service:murano'},
+                ],
+                'requires': [{
+                    'name': 'hypervisor:libvirt:kvm',
+                    'message': 'Requires message'
+                }],
+                'incompatible': [
+                    {
+                        'name': 'storage:*',
+                        'message': 'New storage is incompatible with other'
+                    },
+                    {
+                        'name': 'additional_service:sahara',
+                        'message': 'New storage is incompatible with Sahara'
+                    }
+                ]
+            },
+            {
+                'name': 'hypervisor:new',
+                'label': 'New Hypervisor',
+                'compatible': []
+            },
+            {
+                'name': 'additional_service:ironic-new',
+                'label': 'Ironic New',
+                'incompatible': [{
+                    'name': 'additional_service:*',
+                    'message': 'Alert message'
+                }],
+                'requires': [{
+                    'name': 'storage:test'
+                }]
+            }
+        ]
+
+        with mock.patch('fuel_plugin_builder.validators.base.utils') \
+                as mock_utils:
+            for data in data_sets:
+                mock_utils.parse_yaml.return_value = [data]
+                self.validator.check_components_schema()
--- fuel_plugin_builder/validators/schemas/v4.py
+++ fuel_plugin_builder/validators/schemas/v4.py
@@ -17,8 +17,56 @@
 from fuel_plugin_builder.validators.schemas import SchemaV3
 
 
+COMPONENTS_TYPES_STR = '|'.join(
+    ['hypervisor', 'network', 'storage', 'additional_service'])
+COMPONENT_NAME_PATTERN = \
+    '^({0}):([0-9a-z_-]+:)*[0-9a-z_-]+$'.format(COMPONENTS_TYPES_STR)
+COMPATIBLE_COMPONENT_NAME_PATTERN = \
+    '^({0}):([0-9a-z_-]+:)*([0-9a-z_-]+|(\*)?)$'.format(COMPONENTS_TYPES_STR)
+
+
 class SchemaV4(SchemaV3):
 
     @property
     def package_version(self):
         return {'enum': ['4.0.0']}
+
+    @property
+    def components_items(self):
+        return {
+            'type': 'array',
+            'items': {
+                'type': 'object',
+                'required': ['name'],
+                'properties': {
+                    'name': {
+                        'type': 'string',
+                        'pattern': COMPATIBLE_COMPONENT_NAME_PATTERN
+                    },
+                    'message': {'type': 'string'}
+                }
+            }
+        }
+
+    @property
+    def components_schema(self):
+        return {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'array',
+            'items': {
+                'required': ['name', 'label'],
+                'type': 'object',
+                'additionalProperties': False,
+                'properties': {
+                    'name': {
+                        'type': 'string',
+                        'pattern': COMPONENT_NAME_PATTERN
+                    },
+                    'label': {'type': 'string'},
+                    'description': {'type': 'string'},
+                    'compatible': self.components_items,
+                    'requires': self.components_items,
+                    'incompatible': self.components_items
+                }
+            }
+        }
