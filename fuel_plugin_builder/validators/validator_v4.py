8ac2fee34791bed257022edf69bf344f282b7b56
ekosareva <ekosareva@mirantis.com>
2015-10-26 05:55:28 +0400

    Validation for components metadata in FPB
    Adding validation of components.yaml metadata file for V4 plugins
    
    Change-Id: I7b65371c5330abbb0b724e0b6faca66d033d9898
    Implements: blueprint component-registry

--- fuel_plugin_builder/validators/validator_v4.py
+++ fuel_plugin_builder/validators/validator_v4.py
@@ -14,11 +14,32 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import logging
+from os.path import join as join_path
+
+from fuel_plugin_builder.validators.schemas import SchemaV4
 from fuel_plugin_builder.validators import ValidatorV3
+
+logger = logging.getLogger(__name__)
 
 
 class ValidatorV4(ValidatorV3):
 
+    schema = SchemaV4()
+
+    def __init__(self, *args, **kwargs):
+        super(ValidatorV4, self).__init__(*args, **kwargs)
+        self.components_path = join_path(self.plugin_path, 'components.yaml')
+
     @property
     def basic_version(self):
         return '8.0'
+
+    def check_schemas(self):
+        super(ValidatorV4, self).check_schemas()
+        self.check_components_schema()
+
+    def check_components_schema(self):
+        self.validate_file_by_schema(self.schema.components_schema,
+                                     self.components_path,
+                                     check_file_exists=False)
