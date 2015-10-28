4f54f73040cfb0d3dcdfac6f361392bbfa50c612
ekosareva <ekosareva@mirantis.com>
2015-10-29 01:35:23 +0400

    Example plugin v4
    Add example of V4 plugin
    
    Implements: blueprint component-registry
    Change-Id: I3d08a2c4ac10cccdaf37cea83acb76f6672e8fa9

---
+++ examples/fuel_plugin_example_v4/deployment_scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eux
+
+# It's a script which deploys your plugin
+echo fuel_plugin_example_v4 > /tmp/fuel_plugin_example_v4_sh
+
+OS_NAME=''
+if   grep -i CentOS /etc/issue.net >/dev/null; then
+    OS_NAME='centos';
+elif grep -i Ubuntu /etc/issue.net >/dev/null; then
+    OS_NAME='ubuntu';
+fi
+
+function install_package {
+    if [ $OS_NAME == 'ubuntu' ]; then
+        apt-get install -y fuel-simple-service
+    elif [ $OS_NAME == 'centos' ]; then
+        yum install -y fuel-simple-service
+    fi
+}
+
+install_package
+fuel-simple-service.py &
