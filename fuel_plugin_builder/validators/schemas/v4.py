267ca4ab5c6977bd38a8b8cd1cc4516f85c3d87a
Ilya Kutukov <ikutukov@mirantis.com>
2015-11-25 16:39:46 +0300

    Adds is_runtime flag that allow to install plugin to operational environment.
    Change-Id: I57a9570566184b5c5e8ddd6090a256b79075c61f
    Related-Bug: #1519050

--- fuel_plugin_builder/validators/schemas/v4.py
+++ fuel_plugin_builder/validators/schemas/v4.py
@@ -14,6 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from fuel_plugin_builder import consts
 from fuel_plugin_builder.validators.schemas import SchemaV3
 
 
@@ -22,3 +23,47 @@ class SchemaV4(SchemaV3):
     @property
     def package_version(self):
         return {'enum': ['4.0.0']}
+
+    @property
+    def metadata_schema(self):
+        return {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'title': 'plugin',
+            'type': 'object',
+            'required': [
+                'name',
+                'title',
+                'version',
+                'package_version',
+                'description',
+                'fuel_version',
+                'licenses',
+                'authors',
+                'homepage',
+                'releases',
+                'groups'],
+            'properties': {
+                'name': {
+                    'type': 'string',
+                    'pattern': consts.PLUGIN_NAME_PATTERN},
+                'title': {'type': 'string'},
+                'version': {'type': 'string'},
+                'package_version': self.package_version,
+                'description': {'type': 'string'},
+                'fuel_version': self.list_of_strings,
+                'licenses': self.list_of_strings,
+                'authors': self.list_of_strings,
+                'groups': {'type': 'array', 'uniqueItems': True, 'items':
+                           {'enum':
+                            ['network',
+                             'storage',
+                             'storage::cinder',
+                             'storage::glance',
+                             'hypervisor',
+                             'monitoring']}},
+                'homepage': {'type': 'string'},
+                'releases': {
+                    'type': 'array',
+                    'items': self.plugin_release_schema}},
+                'is_runtime': {'type': 'boolean'}
+        }
