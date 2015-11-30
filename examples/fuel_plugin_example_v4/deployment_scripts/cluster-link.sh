c2a27e9e7bfa8515f72dda9dbfdfac405cd997bc
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-11-30 19:00:38 +0200

    Example of cluster link added
    Change-Id: If38d13c611d19e2d53e6e85ecbea427b1062463f

---
+++ examples/fuel_plugin_example_v4/deployment_scripts/cluster-link.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eux
+
+# It's a script which creates a custom dashboard link
+CLUSTER_ID=`hiera deployment_id`
+MASTER_IP=`hiera master_ip`
+CLUSTER_LINK="/dashboard"
+
+function dashboard_link {
+curl -H "Content-Type: application/json" -X POST \
+-d '{"title":"Dashboard","description":"A Sample Dashboard Link","url":"${CLUSTER_LINK}"}' \
+http://${MASTER_IP}:8000/api/clusters/${CLUSTER_ID}/plugin_links
+}
+
+dashboard_link
