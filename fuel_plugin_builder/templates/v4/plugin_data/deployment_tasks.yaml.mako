0f2e9b28cb37d1b880411e324fc5cf52a8f2d794
Ilya Kutukov <ikutukov@mirantis.com>
2015-12-21 03:26:58 +0300

    Added pre-post tasks validation to plugins builder
    - The plugin deployment tasks now could contain
      cross-depends and cross-depended-by properties
      inroduced by task based deploment feature.
    
    Change-Id: Icb351e4475ba6e78e7c8ef22cefc255116d44ba8
    Related-Bug: #1527325

---
+++ fuel_plugin_builder/templates/v4/plugin_data/deployment_tasks.yaml.mako
@@ -0,0 +1,56 @@
+# These tasks will be merged into deployment graph. Here you
+# can specify new tasks for any roles, even built-in ones.
+
+- id: ${plugin_name}
+  type: group
+  role: [${plugin_name}]
+  parameters:
+    strategy:
+      type: parallel
+
+- id: ${plugin_name}-deployment-puppet
+  version: 2.0.0              # task format version
+  type: puppet
+  role: [${plugin_name}]
+  required_for: [deploy_end]  # for version 1.0 compatibility
+  requires: [deploy_start]    # for version 1.0 compatibility
+  cross-depended-by:
+    - name: deploy_end
+  cross-depends:              # version 2.0 dependency
+    - name: deploy_start
+  parameters:
+    puppet_manifest: "deploy.pp"
+    puppet_modules: "."
+    timeout: 3600
+
+#- id: ${plugin_name}-post-deployment-sh
+#  version: 2.0.0
+#  type: shell
+#  role: [${plugin_name}]
+#  required_for: [post_deployment_end]
+#  requires: [post_deployment_start]
+#  cross-depended-by:
+#    - name: post_deployment_end
+#  cross-depends:
+#    - name: post_deployment_start
+#  parameters:
+#    cmd: echo post_deployment_task_executed > /tmp/post_deployment
+#    retries: 3
+#    interval: 20
+#    timeout: 180
+
+#- id: ${plugin_name}-pre-deployment-sh
+#  version: 2.0.0
+#  type: shell
+#  role: [${plugin_name}]
+#  required_for: [pre_deployment_end]
+#  requires: [pre_deployment_start]
+#  cross-depended-by:
+#    - name: pre_deployment_end
+#  cross-depends:
+#    - name: pre_deployment_start
+#  parameters:
+#    cmd: echo pre_deployment_task_executed > /tmp/pre_deployment
+#    retries: 3
+#    interval: 20
+#    timeout: 180
--- fuel_plugin_builder/validators/schemas/v4.py
+++ fuel_plugin_builder/validators/schemas/v4.py
@@ -14,6 +14,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import six
+
 from fuel_plugin_builder.validators.schemas import SchemaV3
 
 
@@ -25,7 +27,270 @@ COMPATIBLE_COMPONENT_NAME_PATTERN = \
     '^({0}):([0-9a-z_-]+:)*([0-9a-z_-]+|(\*)?)$'.format(COMPONENTS_TYPES_STR)
 
 
+TASK_NAME_PATTERN = TASK_ROLE_PATTERN = '^[0-9a-zA-Z_-]+$'
+NETWORK_ROLE_PATTERN = '^[0-9a-z_-]+$'
+FILE_PERMISSIONS_PATTERN = '^[0-7]{4}$'
+TASK_VERSION_PATTERN = '^\d+.\d+.\d+$'
+STAGE_PATTERN = '^(post_deployment|pre_deployment)' \
+                '(/[-+]?([0-9]*\.[0-9]+|[0-9]+))?$'
+
+
 class SchemaV4(SchemaV3):
+
+    @property
+    def _task_relation(self):
+        return {
+            'type': 'object',
+            'required': ['name'],
+            'properties': {
+                'name': {'type': 'string'},
+                'role': self._task_role,
+                'policy': {
+                    'type': 'string',
+                    'enum': ['all', 'any']
+                }
+            }
+        }
+
+    @property
+    def _task_role(self):
+        return {
+            'oneOf': [
+                {
+                    'type': 'string',
+                    'enum': ['*', 'master', 'self']
+                },
+                {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string',
+                        'pattern': TASK_ROLE_PATTERN
+                    }
+                }
+            ]
+        }
+
+    @property
+    def _task_strategy(self):
+        return {
+            'type': 'object',
+            'properties': {
+                'type': {
+                    'enum': ['parallel', 'one-by-one']
+                }
+            }
+        }
+
+    @property
+    def _task_stage(self):
+        return {'type': 'string', 'pattern': STAGE_PATTERN}
+
+    def _gen_task_schema(
+            self,
+            task_types,
+            required=None,
+            parameters=None):
+        """Generate deployment task schema using prototype.
+
+        :param task_types: task types
+        :type task_types: str|list
+        :param required: new required fields
+        :type required: list
+        :param parameters: new properties dict
+        :type parameters: dict
+        :return:
+        :rtype: dict
+        """
+        if not task_types:
+            raise ValueError('Task type should not be empty')
+
+        if isinstance(task_types, six.string_types):
+            task_types = [task_types]
+
+        return {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'object',
+            'required': list(set(['id', 'type'] + (required or []))),
+            'properties': {
+                'type': {'enum': task_types},
+                'id': {
+                    'type': 'string',
+                    'pattern': TASK_NAME_PATTERN},
+                'version': {
+                    'type': 'string', "pattern": TASK_VERSION_PATTERN},
+                'role': self._task_role,
+                'required_for': self.task_group,
+                'requires': self.task_group,
+                'cross-depends': {
+                    'type': 'array',
+                    'items': self._task_relation},
+                'cross-depended-by': {
+                    'type': 'array',
+                    'items': self._task_relation},
+                'stage': self._task_stage,
+                'tasks': {  # used only for 'group' tasks
+                    'type': 'array',
+                    'items': {
+                        'type': 'string',
+                        'pattern': TASK_ROLE_PATTERN}},
+                'parameters': parameters or {},
+            }
+        }
+
+    @property
+    def deployment_task_schema(self):
+        return {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'array',
+            'items': {
+                "$ref": "#/definitions/anyTask"
+            },
+            "definitions": {
+                "anyTask": self._gen_task_schema(
+                    [
+                        'copy_files',
+                        'group',
+                        'reboot',
+                        'shell',
+                        'skipped',
+                        'stage',
+                        'sync',
+                        'puppet',
+                        'upload_file',
+                    ]
+                )
+            }
+        }
+
+    @property
+    def copy_files_task(self):
+        return self._gen_task_schema(
+            "copy_files",
+            ['role', 'parameters'],
+            {
+                'type': 'object',
+                'required': ['files'],
+                'properties': {
+                    'files': {
+                        'type': 'array',
+                        'minItems': 1,
+                        'items': {
+                            'type': 'object',
+                            'required': ['src', 'dst'],
+                            'properties': {
+                                'src': {'type': 'string'},
+                                'dst': {'type': 'string'}}}},
+                    'permissions': {
+                        'type': 'string',
+                        'pattern': FILE_PERMISSIONS_PATTERN},
+                    'dir_permissions': {
+                        'type': 'string',
+                        'pattern': FILE_PERMISSIONS_PATTERN}}})
+
+    @property
+    def group_task(self):
+        return self._gen_task_schema(
+            "group",
+            ['role'],
+            {
+                'type': 'object',
+                'properties': {
+                    'strategy': self._task_strategy}
+            },
+        )
+
+    @property
+    def puppet_task(self):
+        return self._gen_task_schema(
+            "puppet",
+            [],
+            {
+                'type': 'object',
+                'required': [
+                    'puppet_manifest', 'puppet_modules', 'timeout'],
+                'properties': {
+                    'puppet_manifest': {
+                        'type': 'string', 'minLength': 1},
+                    'puppet_modules': {
+                        'type': 'string', 'minLength': 1},
+                    'timeout': {'type': 'integer'},
+                    'retries': {'type': 'integer'}
+                }
+            }
+        )
+
+    @property
+    def reboot_task(self):
+        return self._gen_task_schema(
+            "reboot",
+            [],
+            {
+                'type': 'object',
+                'properties': {
+                    'timeout': {'type': 'integer'}
+                }
+            }
+        )
+
+    @property
+    def shell_task(self):
+        return self._gen_task_schema(
+            "shell",
+            ['role'],
+            {
+                'type': 'object',
+                'required': ['cmd'],
+                'properties': {
+                    'cmd': {
+                        'type': 'string'},
+                    'retries': {
+                        'type': 'integer'},
+                    'interval': {
+                        'type': 'integer'},
+                    'timeout': {
+                        'type': 'integer'}
+                }
+            }
+        )
+
+    @property
+    def skipped_task(self):
+        return self._gen_task_schema("skipped")
+
+    @property
+    def stage_task(self):
+        return self._gen_task_schema("stage")
+
+    @property
+    def sync_task(self):
+        return self._gen_task_schema(
+            "sync",
+            ['role', 'parameters'],
+            {
+                'type': 'object',
+                'required': ['src', 'dst'],
+                'properties': {
+                    'src': {'type': 'string'},
+                    'dst': {'type': 'string'},
+                    'timeout': {'type': 'integer'}
+                }
+            }
+        )
+
+    @property
+    def upload_file_task(self):
+        return self._gen_task_schema(
+            "upload_file",
+            ['role', 'parameters'],
+            {
+                'type': 'object',
+                'required': ['path', 'data'],
+                'properties': {
+                    'path': {'type': 'string'},
+                    'data': {'type': 'string'}
+                }
+            }
+        )
 
     @property
     def package_version(self):
