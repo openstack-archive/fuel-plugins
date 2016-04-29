a3f9289478e9f9f9706d7166f072d77385e7cecf
Guo Ruijing <ruijing.guo@intel.com>
2016-04-29 13:40:27 +0800

    Support multi versions of package in fuel plugin
    Some plugins may have repeating packages, which are currently filtered
    by dpkg-scanpackages. This is the case for multiarch builds of some Fuel
    plugins like ODL, which build and ship both amd64 and arm64 DEBs for ODL
    prereqs. Allow multiple versions of a single package, by passing the
    `-m` flag to dpkg-scanpackages when building local Ubuntu repo.
    
    Closes-Bug: #1599896
    
    Change-Id: Iba6442d2f21b252769ae30b2af01bbb04e16fd4c
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- fuel_plugin_builder/actions/build.py
+++ fuel_plugin_builder/actions/build.py
@@ -103,7 +103,7 @@ class BaseBuildPlugin(BaseAction):
     def build_ubuntu_repos(cls, releases_paths):
         for repo_path in releases_paths:
             utils.exec_piped_cmds(
-                ['dpkg-scanpackages .', 'gzip -c9 > Packages.gz'],
+                ['dpkg-scanpackages -m .', 'gzip -c9 > Packages.gz'],
                 cwd=repo_path)
 
     @classmethod
@@ -227,7 +227,7 @@ class BuildPluginV2(BaseBuildPlugin):
     def build_ubuntu_repos(self, releases_paths):
         for repo_path in releases_paths:
             utils.exec_piped_cmds(
-                ['dpkg-scanpackages .', 'gzip -c9 > Packages.gz'],
+                ['dpkg-scanpackages -m .', 'gzip -c9 > Packages.gz'],
                 cwd=repo_path)
             release_path = join_path(repo_path, 'Release')
             utils.render_to_file(
