61d6851146d12f6a76c20ec82cbaef4bb52106e1
Ilya Kutukov <ikutukov@mirantis.com>
2016-08-09 07:17:21 +0300

    Fuel plugin builder v5
    - Now it is possible to deliver Fuel release configuration using the Fuel plugin
      [bp/release-as-a-plugin](https://blueprints.launchpad.net/fuel/+spec/release-as-a-plugin)
      Using flag ``is_release: true`` in ``metadata.yaml: releases:`` section you coud
      define new fuel release.
      Also you could define ``base_release: release_template.yaml`` inside release
      description to share single template between multiple releases.
    - ``*_path`` directive in is now supported in ``metadata.yaml``.
      Now you could require folder, external file or merged glob output using keys
      like ``deployment_tasks_path: 'dt.yaml'``
    - It is possible to define graphs linked with plugin or releases
      directive inside ``metadata.yaml``, see the examples.
    - All yaml root files is not not required except ``metadata.yaml``.
    - Templates and examples for the Fuel plugins package ``v5.0.0`` are added.
    - Fuel plugin builder is refactored to make all configurations traceable and
      decouple logic working with file system with validation logic and building
      logic.
      [LP1539598](https://bugs.launchpad.net/fuel/+bug/1539598)
    - Docker environment for building and creating plugins example.
    - Experimental ``JSON`` manifests support added.
    - Schemas are aligned with Fuel versions.
    - Advanced build process reporting.
      Now FPB trying to detect all possible problems in plugin configuration
      and report them instead of failing of first of them and also could warn user
      without stopping execution.
    - Now it is possible to build plugin package v4.0.0 without ``tasks.yaml``
    
    Change-Id: I55d0313db7cd64ab16802a75ff0d9edd16782d01
    Implements-blueprint: release-as-a-plugin
    Closes-Bug: #1539598
    Closes-Bug: #1552248

---
+++ fuel_plugin_builder/schemas/__init__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+#
+# Please, be kindly reminded about data schema versioning policy and its
+# nuances:
+#
+# Fuel Plugin Builder(FPB) is on intersection of several lines of versioning
+# subsystems and APIs making the data schema tricky (as small as it possible)
+#
+# ``Tasks`` version that are defined by changes in data flow chain
+# between of API Fuel, Astute, Fuel Library and QA environment. In theory, all
+# versions of this tasks could be delivered now, on practice v1.0.0 is not
+# even close to way you may want to configure what Nailgun do.
+#
+# ``Plugin package`` version as well FPB version is semver where major going
+# up on all changes in plugin format and any changes of data schema inside it
+# that could affect internal and third-party plugins developers.
+#
+# ``FPB own version`` FPB not releasing together with Fuel, but respecting
+# Fuel milestones. Fuel still holds a great backward compatibility with plugins
+# 3 and even more major releases ago, so there is no rush to roll up changes
+# that will open new sometimes experimental functionality. Everyone who want
+# to work with new features is free to clone and use latest master of FPB to
+# build new format of plugin packages.
+#
+#
+# So we have hypothetical versions snapshot:
+#
+# FPB version 4.1.0
+# Plugins package version 4.0.0
+# Fuel version 9.0.1
+# Tasks version 2.1.0
+#
+
+from .attributes import *
+from .common import *
+from .components import *
+from .graph import *
+from .metadata import *
+from .network_roles import *
+from .node_roles import *
+from .release import *
+from .task import *
+from .volumes import *
+
