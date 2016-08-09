4e91668ba5cc8337fd815e7f7c3122c0eb7a3737
Ilya Kutukov <ikutukov@mirantis.com>
2016-08-09 07:17:21 +0300

    Fuel plugin builder v5
    - Now it is possible to deliver Fuel release configuration using the Fuel plugin
      [bp/release-as-a-plugin](https://blueprints.launchpad.net/fuel/+spec/release-as-a-plugin)
      Using flag ``is_release: true`` in ``metadata.yaml: releases:`` section you coud
      define new fuel release.
      Also you could define ``base_release: release_template.yaml`` inside release
      description to share single template between multiple releases.
    - ``*_path`` directive in is now supported in ``metadata.yaml``.
      Now you could require folder, external file or merged glob output using keys
      like ``deployment_tasks_path: 'dt.yaml'``
    - It is possible to define graphs linked with plugin or releases
      directive inside ``metadata.yaml``, see the examples.
    - All yaml root files is not not required except ``metadata.yaml``.
    - Templates and examples for the Fuel plugins package ``v5.0.0`` are added.
    - Fuel plugin builder is refactored to make all configurations traceable and
      decouple logic working with file system with validation logic and building
      logic.
      [LP1539598](https://bugs.launchpad.net/fuel/+bug/1539598)
    - Docker environment for building and creating plugins example.
    - Experimental ``JSON`` manifests support added.
    - Schemas are aligned with Fuel versions.
    - Advanced build process reporting.
      Now FPB trying to detect all possible problems in plugin configuration
      and report them instead of failing of first of them and also could warn user
      without stopping execution.
    - Now it is possible to build plugin package v4.0.0 without ``tasks.yaml``
    
    Change-Id: I55d0313db7cd64ab16802a75ff0d9edd16782d01
    Implements-blueprint: release-as-a-plugin
    Closes-Bug: #1539598
    Closes-Bug: #1552248

---
+++ fuel_plugin_builder/tests/test_validator_v5.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuel_plugin_builder import loaders
+from fuel_plugin_builder.tests.base import FakeFSTest
+from fuel_plugin_builder import validators
+
+
+class TestValidatorV5(FakeFSTest):
+    validator_class = validators.ValidatorV5
+    loader_class = loaders.PluginLoaderV5
+    package_version = '5.0.0'
+
+    __test__ = True
+
+    def test_validate(self):
+        report = self.validator.validate(self.data_tree)
+        self.assertIn(u'Success!', report.render())
+
+    def test_fuel_version_legacy_warning(self):
+        self.data_tree.update(
+            self._make_fake_metadata_data(fuel_version=['9.1'])
+        )
+        report = self.validator.validate(self.data_tree)
+        self.assertIn(u'WARNING: "fuel_version" field in metadata.yaml is '
+                      u'deprecated and will be removed in further Fuel '
+                      u'releases.', report.render())
+        self.assertFalse(report.is_failed())
+
+    def test_check_tasks_schema_validation_failed(self):
+        bad_tasks_data = [
+            {
+                'type': 'shell',
+                'parameters': {
+                    'timeout': 3
+                },
+                'stage': 'post_deployment',
+                'role': '*'
+            },
+            {
+                'type': 'puppet',
+                'parameters': {
+                    'timeout': 3
+                },
+                'stage': 'post_deployment',
+                'role': '*'
+            },
+            {
+                'parameters': {
+                    'timeout': 3,
+                    'cmd': 'xx'
+                },
+                'stage': 'post_deployment',
+                'role': '*'
+            },
+            {
+                'type': 'shell',
+                'parameters': {
+                    'timeout': 3,
+                    'puppet_manifest': 'xx',
+                    'puppet_modules': 'yy',
+                },
+                'stage': 'post_deployment',
+                'role': '*'
+            },
+            {
+                'type': 'puppet',
+                'parameters': {
+                    'timeout': 3,
+                    'puppet_manifest': 'xx',
+                    'puppet_modules': 'yy',
+                    'retries': 'asd',
+                },
+                'stage': 'post_deployment',
+                'role': '*'
+            },
+            {
+                'type': 'puppet',
+                'parameters': {
+                    'timeout': 3,
+                    'puppet_manifest': 'xx',
+                    'puppet_modules': '',
+                    'retries': 1,
+                },
+                'stage': 'pre_deployment',
+                'role': '*'
+            },
+            {
+                'type': 'puppet',
+                'parameters': {
+                    'timeout': 3,
+                    'puppet_manifest': '',
+                    'puppet_modules': 'yy',
+                    'retries': 1,
+                },
+                'stage': 'pre_deployment',
+                'role': '*'
+            }
+        ]
+        self.data_tree['releases'][0]['graphs'][0]['tasks'] = \
+            bad_tasks_data
+        report = self.validator.validate(self.data_tree)
+        self.assertEqual(report.count_failures(), 7 + 1)
+        self.assertIn('Failure!', report.render())
+
+    def test_check_tasks_schema_validation_passed(self):
+        data_sets = [
+            [
+                {
+                    'id': 'test1',
+                    'type': 'shell',
+                    'parameters': {
+                        'timeout': 3,
+                        'cmd': 'xx'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                },
+            ],
+            [
+                {
+                    'id': 'test1',
+                    'type': 'shell',
+                    'parameters': {
+                        'timeout': 3,
+                        'cmd': 'xx'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                },
+                {
+                    'id': 'test2',
+                    'type': 'puppet',
+                    'parameters': {
+                        'timeout': 3,
+                        'puppet_manifest': 'xx',
+                        'puppet_modules': 'xxx'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                },
+            ],
+            [
+                {
+                    'id': 'test3',
+                    'type': 'shell',
+                    'parameters': {
+                        'timeout': 3,
+                        'cmd': 'reboot'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                },
+                {
+                    'id': 'test4',
+                    'type': 'shell',
+                    'parameters': {
+                        'timeout': 3,
+                        'cmd': 'xx'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                },
+                {
+                    'id': 'test5',
+                    'type': 'puppet',
+                    'parameters': {
+                        'timeout': 3,
+                        'puppet_manifest': 'xx',
+                        'puppet_modules': 'xxx'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                }
+            ],
+            [
+                {
+                    'id': 'test1',
+                    'type': 'shell',
+                    'parameters': {
+                        'timeout': 3,
+                        'cmd': 'reboot'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                },
+                {
+                    'id': 'test2',
+                    'type': 'shell',
+                    'parameters': {
+                        'timeout': 3,
+                        'puppet_manifest': 'xx',
+                        'puppet_modules': 'yy',
+                        'cmd': 'reboot'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                },
+                {
+                    'id': 'test3',
+                    'type': 'puppet',
+                    'parameters': {
+                        'timeout': 3,
+                        'retries': 10,
+                        'puppet_manifest': 'xx',
+                        'puppet_modules': 'xxx'
+                    },
+                    'stage': 'post_deployment',
+                    'role': '*'
+                },
+                {
+                    'id': 'test4',
+                    'type': 'puppet',
+                    'parameters': {
+                        'timeout': 3,
+                        'retries': 10,
+                        'puppet_manifest': 'xx',
+                        'puppet_modules': 'xxx'
+                    },
+                    'stage': 'post_deployment',
+                    'role': 'master'
+                },
+            ]
+        ]
+        for data in data_sets:
+            self.data_tree['releases'][0]['graphs'][0]['tasks'] = data
+            report = self.validator.validate(self.data_tree)
+            self.assertFalse(report.is_failed())
+            self.assertIn('Success!', report.render())
