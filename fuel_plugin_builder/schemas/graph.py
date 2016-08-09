e5d92d0d19c54f43455f1eab6191f66f55c17a70
Ilya Kutukov <ikutukov@mirantis.com>
2016-08-09 07:17:21 +0300

    Fuel plugin builder v5
    - Now it is possible to deliver Fuel release configuration using the Fuel plugin
      [bp/release-as-a-plugin](https://blueprints.launchpad.net/fuel/+spec/release-as-a-plugin)
      Using flag ``is_release: true`` in ``metadata.yaml: releases:`` section you coud
      define new fuel release.
      Also you could define ``base_release: release_template.yaml`` inside release
      description to share single template between multiple releases.
    - ``*_path`` directive in is now supported in ``metadata.yaml``.
      Now you could require folder, external file or merged glob output using keys
      like ``deployment_tasks_path: 'dt.yaml'``
    - It is possible to define graphs linked with plugin or releases
      directive inside ``metadata.yaml``, see the examples.
    - All yaml root files is not not required except ``metadata.yaml``.
    - Templates and examples for the Fuel plugins package ``v5.0.0`` are added.
    - Fuel plugin builder is refactored to make all configurations traceable and
      decouple logic working with file system with validation logic and building
      logic.
      [LP1539598](https://bugs.launchpad.net/fuel/+bug/1539598)
    - Docker environment for building and creating plugins example.
    - Experimental ``JSON`` manifests support added.
    - Schemas are aligned with Fuel versions.
    - Advanced build process reporting.
      Now FPB trying to detect all possible problems in plugin configuration
      and report them instead of failing of first of them and also could warn user
      without stopping execution.
    - Now it is possible to build plugin package v4.0.0 without ``tasks.yaml``
    
    Change-Id: I55d0313db7cd64ab16802a75ff0d9edd16782d01
    Implements-blueprint: release-as-a-plugin
    Closes-Bug: #1539598
    Closes-Bug: #1552248

---
+++ fuel_plugin_builder/schemas/graph.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from task import task_v2_2_0
+
+
+class SchemaGraphV9_1(object):
+    graph = {
+        '$schema': 'http://json-schema.org/draft-04/schema#',
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {
+            'metadata': {'type': 'object'},
+            'name': {'type': 'string'},
+            'tasks': task_v2_2_0.tasks
+        }
+    }
+
+    @property
+    def graph_association(self):
+        return {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'object',
+            'additionalProperties': False,
+            'required': ['graph'],
+            'properties': {
+                'graph': self.graph,
+                'type': {'type': 'string'}
+            }
+        }
+
+
+graph_v9_1 = SchemaGraphV9_1()
