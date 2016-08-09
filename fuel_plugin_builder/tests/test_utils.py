e14c0225d5edcdc4ed88c3e9fbf6f64705cf8a34
Ilya Kutukov <ikutukov@mirantis.com>
2016-08-09 07:17:21 +0300

    Fuel plugin builder v5
    - Now it is possible to deliver Fuel release configuration using the Fuel plugin
      [bp/release-as-a-plugin](https://blueprints.launchpad.net/fuel/+spec/release-as-a-plugin)
      Using flag ``is_release: true`` in ``metadata.yaml: releases:`` section you coud
      define new fuel release.
      Also you could define ``base_release: release_template.yaml`` inside release
      description to share single template between multiple releases.
    - ``*_path`` directive in is now supported in ``metadata.yaml``.
      Now you could require folder, external file or merged glob output using keys
      like ``deployment_tasks_path: 'dt.yaml'``
    - It is possible to define graphs linked with plugin or releases
      directive inside ``metadata.yaml``, see the examples.
    - All yaml root files is not not required except ``metadata.yaml``.
    - Templates and examples for the Fuel plugins package ``v5.0.0`` are added.
    - Fuel plugin builder is refactored to make all configurations traceable and
      decouple logic working with file system with validation logic and building
      logic.
      [LP1539598](https://bugs.launchpad.net/fuel/+bug/1539598)
    - Docker environment for building and creating plugins example.
    - Experimental ``JSON`` manifests support added.
    - Schemas are aligned with Fuel versions.
    - Advanced build process reporting.
      Now FPB trying to detect all possible problems in plugin configuration
      and report them instead of failing of first of them and also could warn user
      without stopping execution.
    - Now it is possible to build plugin package v4.0.0 without ``tasks.yaml``
    
    Change-Id: I55d0313db7cd64ab16802a75ff0d9edd16782d01
    Implements-blueprint: release-as-a-plugin
    Closes-Bug: #1539598
    Closes-Bug: #1552248

--- fuel_plugin_builder/tests/test_utils.py
+++ fuel_plugin_builder/tests/test_utils.py
@@ -29,31 +29,35 @@ from fuel_plugin_builder import utils
 
 
 class TestUtils(BaseTestCase):
-
-    @mock.patch('fuel_plugin_builder.utils.os.path.isfile', return_value=True)
-    @mock.patch('fuel_plugin_builder.utils.os.access', return_value=True)
+    @mock.patch('fuel_plugin_builder.utils.fs.os.path.isfile',
+                return_value=True)
+    @mock.patch('fuel_plugin_builder.utils.fs.os.access',
+                return_value=True)
     def test_is_executable_returns_true(self, access_mock, isfile_mock):
         file_name = 'file_name'
         self.assertTrue(utils.is_executable(file_name))
         isfile_mock.assert_called_once_with(file_name)
         access_mock.assert_called_once_with(file_name, os.X_OK)
 
-    @mock.patch('fuel_plugin_builder.utils.os.path.isfile', return_value=True)
-    @mock.patch('fuel_plugin_builder.utils.os.access', return_value=False)
+    @mock.patch('fuel_plugin_builder.utils.fs.os.path.isfile',
+                return_value=True)
+    @mock.patch('fuel_plugin_builder.utils.fs.os.access',
+                return_value=False)
     def test_is_executable_returns_false(self, access_mock, isfile_mock):
         file_name = 'file_name'
         self.assertFalse(utils.is_executable(file_name))
         isfile_mock.assert_called_once_with(file_name)
         access_mock.assert_called_once_with(file_name, os.X_OK)
 
-    @mock.patch('fuel_plugin_builder.utils.os')
-    @mock.patch('fuel_plugin_builder.utils.is_executable', return_value=True)
+    @mock.patch('fuel_plugin_builder.utils.fs.os')
+    @mock.patch('fuel_plugin_builder.utils.fs.is_executable',
+                return_value=True)
     def test_which_returns_for_absolute_path_exec(self, _, os_mock):
         path = '/usr/bin/some_exec'
         os_mock.path.split.return_value = ('/usr/bin/', 'some_exec')
         self.assertEqual(utils.which(path), path)
 
-    @mock.patch('fuel_plugin_builder.utils.is_executable',
+    @mock.patch('fuel_plugin_builder.utils.fs.is_executable',
                 side_effect=[False, True])
     def test_which_returns_if_exec_in_env_path(self, _):
         # some_exec is in /bin directory
@@ -61,7 +65,8 @@ class TestUtils(BaseTestCase):
         with patch.dict('os.environ', {'PATH': '/usr/bin:/bin'}):
             self.assertEqual(utils.which(path), '/bin/some_exec')
 
-    @mock.patch('fuel_plugin_builder.utils.is_executable', return_value=False)
+    @mock.patch('fuel_plugin_builder.utils.is_executable',
+                return_value=False)
     def test_which_returns_none(self, _):
         with patch.dict('os.environ', {'PATH': '/usr/bin:/bin'}):
             self.assertIsNone(utils.which('some_exec'))
@@ -110,7 +115,7 @@ class TestUtils(BaseTestCase):
             utils.exec_piped_cmds(['some command', 'some other command'])
             process_mock.communicate.assert_called_with(input='stdout')
 
-    @mock.patch('fuel_plugin_builder.utils.os')
+    @mock.patch('fuel_plugin_builder.utils.fs.os')
     def test_create_dir(self, os_mock):
         path = '/dir/path'
         os_mock.path.isdir.return_value = False
@@ -118,7 +123,7 @@ class TestUtils(BaseTestCase):
         os_mock.path.isdir.assert_called_once_with(path)
         os_mock.makedirs.assert_called_once_with(path)
 
-    @mock.patch('fuel_plugin_builder.utils.os')
+    @mock.patch('fuel_plugin_builder.utils.fs.os')
     def test_create_dir_dont_create_if_created(self, os_mock):
         path = '/dir/path'
         os_mock.path.isdir.return_value = True
@@ -126,33 +131,34 @@ class TestUtils(BaseTestCase):
         os_mock.path.isdir.assert_called_once_with(path)
         self.method_was_not_called(os_mock.makedirs)
 
-    @mock.patch('fuel_plugin_builder.utils.os.path.lexists', return_value=True)
+    @mock.patch('fuel_plugin_builder.utils.fs.os.path.lexists',
+                return_value=True)
     def test_exists(self, os_exists):
         file_path = '/dir/path'
-        self.assertTrue(utils.exists(file_path))
+        self.assertTrue(utils.fs.is_exists(file_path))
         os_exists.assert_called_once_with(file_path)
 
-    @mock.patch('fuel_plugin_builder.utils.os.path.lexists',
+    @mock.patch('fuel_plugin_builder.utils.fs.os.path.lexists',
                 return_value=False)
     def test_exists_returns_false(self, os_exists):
         file_path = '/dir/path'
-        self.assertFalse(utils.exists(file_path))
+        self.assertFalse(utils.fs.is_exists(file_path))
         os_exists.assert_called_once_with(file_path)
 
-    @mock.patch('fuel_plugin_builder.utils.os.path.basename')
+    @mock.patch('fuel_plugin_builder.utils.fs.os.path.basename')
     def test_basename(self, base_mock):
         path = 'some_path'
         base_mock.return_value = path
         self.assertEqual(utils.basename(path), path)
         base_mock.assert_called_once_with(path)
 
-    @mock.patch('fuel_plugin_builder.utils.shutil')
+    @mock.patch('fuel_plugin_builder.utils.fs.shutil')
     def test_copy_file_permissions(self, shutil_mock):
         utils.copy_file_permissions('src', 'dst')
         shutil_mock.copymode.assert_called_once_with('src', 'dst')
 
-    @mock.patch('fuel_plugin_builder.utils.shutil')
-    @mock.patch('fuel_plugin_builder.utils.os')
+    @mock.patch('fuel_plugin_builder.utils.fs.shutil')
+    @mock.patch('fuel_plugin_builder.utils.fs.os')
     def test_remove_file(self, os_mock, shutil_mock):
         path = 'file_for_removing'
         os_mock.path.isdir.return_value = False
@@ -160,8 +166,8 @@ class TestUtils(BaseTestCase):
         os_mock.remove.assert_called_once_with(path)
         self.method_was_not_called(shutil_mock.rmtree)
 
-    @mock.patch('fuel_plugin_builder.utils.shutil')
-    @mock.patch('fuel_plugin_builder.utils.os')
+    @mock.patch('fuel_plugin_builder.utils.fs.shutil')
+    @mock.patch('fuel_plugin_builder.utils.fs.os')
     def test_remove_dir(self, os_mock, shutil_mock):
         path = 'dir_for_removing'
         os_mock.path.isdir.return_value = True
@@ -170,33 +176,33 @@ class TestUtils(BaseTestCase):
         shutil_mock.rmtree.assert_called_once_with(path)
         self.method_was_not_called(os_mock.remove)
 
-    @mock.patch('fuel_plugin_builder.utils.dir_util')
-    @mock.patch('fuel_plugin_builder.utils.shutil')
-    @mock.patch('fuel_plugin_builder.utils.os')
-    def test_copy_file(self, os_mock, shutil_mock, dir_util_mock):
+    @mock.patch('fuel_plugin_builder.utils.fs.dir_util')
+    @mock.patch('fuel_plugin_builder.utils.fs.shutil')
+    @mock.patch('fuel_plugin_builder.utils.fs.os')
+    def test_copy_file(self, os_mock, shutil_mock, dir_util_m):
         src = '/tmp/soruce_file'
         dst = '/tmp/destination_file'
         os_mock.path.isdir.return_value = False
         utils.copy(src, dst)
         shutil_mock.copy.assert_called_once_with(src, dst)
-        self.method_was_not_called(dir_util_mock.copy_tree)
+        self.method_was_not_called(dir_util_m.copy_tree)
 
-    @mock.patch('fuel_plugin_builder.utils.dir_util')
-    @mock.patch('fuel_plugin_builder.utils.shutil')
-    @mock.patch('fuel_plugin_builder.utils.os')
-    def test_copy_dir(self, os_mock, shutil_mock, dir_util_mock):
+    @mock.patch('fuel_plugin_builder.utils.fs.dir_util')
+    @mock.patch('fuel_plugin_builder.utils.fs.shutil')
+    @mock.patch('fuel_plugin_builder.utils.fs.os')
+    def test_copy_dir(self, is_dir_mock, shutil_mock, dir_util_m):
         src = '/tmp/soruce_file'
         dst = '/tmp/destination_file'
-        os_mock.path.isdir.return_value = True
-        utils.copy(src, dst)
-        dir_util_mock.copy_tree.assert_called_once_with(
+        is_dir_mock.return_value = True
+        utils.fs.copy(src, dst)
+        dir_util_m.copy_tree.assert_called_once_with(
             src,
             dst,
             preserve_symlinks=True)
         self.method_was_not_called(shutil_mock.copy)
 
-    @mock.patch('fuel_plugin_builder.utils.copy')
-    @mock.patch('fuel_plugin_builder.utils.glob',
+    @mock.patch('fuel_plugin_builder.utils.fs.copy')
+    @mock.patch('fuel_plugin_builder.utils.fs.glob.glob',
                 return_value=['file1', 'file2'])
     def test_copy_files_in_dir(self, glob_mock, copy_mock):
         mask = 'file*'
@@ -208,7 +214,7 @@ class TestUtils(BaseTestCase):
             [mock.call('file1', '/tmp/file1'),
              mock.call('file2', '/tmp/file2')])
 
-    @mock.patch('fuel_plugin_builder.utils.tarfile')
+    @mock.patch('fuel_plugin_builder.utils.fs.tarfile')
     def test_make_tar_gz(self, tarfile_mock):
         src = 'dir'
         dst = '/tmp/file.fp'
@@ -220,8 +226,8 @@ class TestUtils(BaseTestCase):
         tar_mock.add.assert_called_once_with(src, arcname=prefix)
         tar_mock.close.assert_called_once_with()
 
-    @mock.patch('fuel_plugin_builder.utils.shutil.move')
-    @mock.patch('fuel_plugin_builder.utils.glob',
+    @mock.patch('fuel_plugin_builder.utils.fs.shutil.move')
+    @mock.patch('fuel_plugin_builder.utils.fs.glob.glob',
                 return_value=['file1', 'file2'])
     def test_move_files_in_dir(self, glob_mock, move_mock):
         mask = 'file*'
@@ -233,19 +239,9 @@ class TestUtils(BaseTestCase):
             [mock.call('file1', '/tmp/file1'),
              mock.call('file2', '/tmp/file2')])
 
-    @mock.patch('__builtin__.open')
-    @mock.patch('fuel_plugin_builder.utils.yaml')
-    def test_parse_yaml(self, yaml_mock, open_mock):
-        path = '/tmp/path'
-        file_mock = mock.MagicMock()
-        open_mock.return_value = file_mock
-        utils.parse_yaml(path)
-        open_mock.assert_called_once_with(path)
-        yaml_mock.load.assert_called_once_with(file_mock)
-
     def test_render_to_file_unicode_handling(self):
         expected = u'тест'
-        params = {'vendors': expected}
+        context = {'vendors': expected}
         template_content = "${vendors}"
 
         temp_dir = tempfile.mkdtemp()
@@ -257,16 +253,18 @@ class TestUtils(BaseTestCase):
         with open(src_file, 'w') as f:
             f.write(template_content)
 
-        utils.render_to_file(src=src_file, dst=dst_file, params=params)
+        utils.template.load_template_and_render_to_file(
+            src=src_file, dst=dst_file, context=context)
 
         with open(dst_file, 'rb') as f:
             actual = f.read()
             self.assertEqual(expected, actual.decode('utf-8'))
 
-    @mock.patch('fuel_plugin_builder.utils.copy_file_permissions')
-    @mock.patch('fuel_plugin_builder.utils.render_to_file')
-    @mock.patch('fuel_plugin_builder.utils.remove')
-    @mock.patch('fuel_plugin_builder.utils.os.walk')
+    @mock.patch('fuel_plugin_builder.utils.template.copy_file_permissions')
+    @mock.patch(
+        'fuel_plugin_builder.utils.template.load_template_and_render_to_file')
+    @mock.patch('fuel_plugin_builder.utils.template.remove')
+    @mock.patch('fuel_plugin_builder.utils.fs.os.walk')
     def test_render_files_in_dir(
             self, walk_mock, remove_mock, render_mock, copy_permissions_mock):
         dir_path = '/tmp/some_plugin'
@@ -298,19 +296,9 @@ class TestUtils(BaseTestCase):
                        '/tmp/some_plugin/file4')],
             copy_permissions_mock.call_args_list)
 
-    def test_calculate_sha(self):
-        file_path = '/tmp/file'
-
-        with mock.patch('__builtin__.open',
-                        self.mock_open('fake file content')):
-
-            self.assertEqual(
-                utils.calculate_sha(file_path),
-                '5083c27641e7e4ae287d690cb3fafb4dd6e8f6ab')
-
-    @mock.patch('fuel_plugin_builder.utils.calculate_sha')
-    @mock.patch('fuel_plugin_builder.utils.os.walk')
-    def test_calculate_checksums(self, walk_mock, sha_mock):
+    @mock.patch('fuel_plugin_builder.utils.checksum.calculate_file_sha')
+    @mock.patch('fuel_plugin_builder.utils.fs.os.walk')
+    def test_calculate_file_checksums(self, walk_mock, sha_mock):
         dir_path = '/tmp/dir_path'
         walk_mock.return_value = [
             [dir_path, '', ['file1.txt', 'file2.txt']],
@@ -319,7 +307,7 @@ class TestUtils(BaseTestCase):
         sha_mock.side_effect = ['sha_1', 'sha_2', 'sha_3']
 
         self.assertEqual(
-            utils.calculate_checksums(dir_path),
+            utils.checksum.calculate_file_checksums(dir_path),
             [{'file_path': 'file1.txt', 'checksum': 'sha_1'},
              {'file_path': 'file2.txt', 'checksum': 'sha_2'},
              {'file_path': 'file3.txt', 'checksum': 'sha_3'}])
@@ -330,7 +318,7 @@ class TestUtils(BaseTestCase):
              mock.call('/tmp/dir_path/file3.txt')],
             sha_mock.call_args_list)
 
-    @mock.patch('fuel_plugin_builder.utils.calculate_checksums')
+    @mock.patch('fuel_plugin_builder.utils.checksum.calculate_file_checksums')
     def test_create_checksums_file(self, calculate_mock):
         calculate_mock.return_value = [
             {'checksum': 'checksum2', 'file_path': 'file2.txt'},
@@ -346,8 +334,8 @@ class TestUtils(BaseTestCase):
             fileobj.getvalue(),
             'checksum file1.txt\nchecksum2 file2.txt\n')
 
-    @mock.patch('fuel_plugin_builder.utils.remove')
-    @mock.patch('fuel_plugin_builder.utils.glob',
+    @mock.patch('fuel_plugin_builder.utils.fs.remove')
+    @mock.patch('fuel_plugin_builder.utils.fs.glob.glob',
                 return_value=['file1', 'file2'])
     def test_remove_by_mask(self, glob_mock, remove_mock):
         mask = '/tmp/test/*.yaml'
@@ -356,19 +344,3 @@ class TestUtils(BaseTestCase):
         self.assertEqual(
             remove_mock.call_args_list,
             [mock.call('file1'), mock.call('file2')])
-
-    @mock.patch('fuel_plugin_builder.utils.exists',
-                return_value=True)
-    def test_read_if_exist(self, utils_exists):
-        file_path = '/tmp/file'
-        with mock.patch('__builtin__.open', self.mock_open("foo")):
-            self.assertEqual(utils.read_if_exist(file_path), "foo")
-        utils_exists.assert_called_once_with(file_path)
-
-    @mock.patch('fuel_plugin_builder.utils.exists',
-                return_value=False)
-    def test_read_if_exist_returns_empty(self, utils_exists):
-        file_path = '/tmp/file'
-        with mock.patch('__builtin__.open', self.mock_open("foo")):
-            self.assertEqual(utils.read_if_exist(file_path), "")
-        utils_exists.assert_called_once_with(file_path)
