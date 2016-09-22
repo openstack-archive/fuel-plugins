500908362c073a44ac3986b4c8acbae7e7bc0003
Elena Kosareva <ekosareva@mirantis.com>
2016-09-22 11:36:57 +0300

    Fuel plugin builder v5
    - Now it is possible to deliver Fuel release configuration using the Fuel plugin
      [bp/release-as-a-plugin](https://blueprints.launchpad.net/fuel/+spec/release-as-a-plugin)
      Using flag ``is_release: true`` in ``metadata.yaml: releases:`` section you coud
      define new fuel release.
      Also you could define ``base_release: release_template.yaml`` inside release
      description to share single template between multiple releases.
    - ``*_path`` directive in is now supported in ``metadata.yaml``.
      Now you could require folder, external file or merged glob output using keys
      like ``deployment_tasks_path: 'dt.yaml'``
    - It is possible to define graphs linked with plugin or releases
      directive inside ``metadata.yaml``, see the examples.
    - All yaml root files is not not required except ``metadata.yaml``.
    - Templates and examples for the Fuel plugins package ``v5.0.0`` are added.
    - Fuel plugin builder is refactored to make all configurations traceable and
      decouple logic working with file system with validation logic and building
      logic.
      [LP1539598](https://bugs.launchpad.net/fuel/+bug/1539598)
    - Docker environment for building and creating plugins example.
    - Experimental ``JSON`` manifests support added.
    - Schemas are aligned with Fuel versions.
    - Advanced build process reporting.
      Now FPB trying to detect all possible problems in plugin configuration
      and report them instead of failing of first of them and also could warn user
      without stopping execution.
    - Now it is possible to build plugin package v4.0.0 without ``tasks.yaml``
    
    Change-Id: I55d0313db7cd64ab16802a75ff0d9edd16782d01
    Implements-blueprint: release-as-a-plugin
    Closes-Bug: #1539598
    Closes-Bug: #1552248

---
+++ fuel_plugin_builder/builders/__init__.py
@@ -0,0 +1,4 @@
+from .builder_base import PluginBuilderBase
+from .builder_v1 import PluginBuilderV1
+from .builder_v2 import PluginBuilderV2
+from .builder_v3 import PluginBuilderV3
--- fuel_plugin_builder/validators/validator_v5.py
+++ fuel_plugin_builder/validators/validator_v5.py
@@ -14,38 +14,81 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from os.path import join as join_path
+from fuel_plugin_builder import checks
+from fuel_plugin_builder import schemas
+from fuel_plugin_builder import utils
+from fuel_plugin_builder.validators.validator_base import ValidatorBase
 
-from fuel_plugin_builder.validators.schemas import SchemaV5
-from fuel_plugin_builder.validators import ValidatorV4
 
+class ValidatorV5(ValidatorBase):
+    package_version = '5.0.0'
+    minimal_fuel_version = '9.1'
 
-class ValidatorV5(ValidatorV4):
+    _data_tree_schemas = {
+        '': schemas.metadata_v9_1.schema,
+        'tasks': schemas.task_v0_0_2.tasks,
+        'deployment_tasks': schemas.task_v2_1_0.tasks,
+        'network_roles_metadata': schemas.network_roles_v8_0.schema,
+        'node_roles_metadata': schemas.node_roles_v7_0.schema,
+        'volumes_metadata': schemas.volumes_v7_0.schema,
+        'components_metadata': schemas.components_v8_0.schema,
+        'node_attributes_metadata': (
+            schemas.node_attributes_v9_1.node_attributes
+        ),
+        'nic_attributes_metadata': (
+            schemas.node_attributes_v9_1.node_nic_attributes
+        ),
+        'bond_attributes_metadata': (
+            schemas.node_attributes_v9_1.node_nic_attributes
+        )
+    }
+    _data_tree_multi_schemas = {
+        'tasks': {
+            'puppet': schemas.task_v0_0_0.puppet_task,
+            'shell': schemas.task_v0_0_0.shell_task,
+            'reboot': schemas.task_v1_0_0.reboot_task
+        },
+        'deployment_tasks': {
+            'puppet': schemas.task_v2_1_0.puppet_task,
+            'shell': schemas.task_v2_1_0.shell_task,
+            'group': schemas.task_v2_1_0.group_task,
+            'skipped': schemas.task_v2_1_0.skipped_task,
+            'copy_files': schemas.task_v2_1_0.copy_files_task,
+            'sync': schemas.task_v2_1_0.sync_task,
+            'upload_file': schemas.task_v2_1_0.upload_file_task,
+            'stage': schemas.task_v2_1_0.stage_task,
+            'reboot': schemas.task_v2_1_0.reboot_task,
+            'move_to_bootstrap': schemas.task_v2_2_0.move_to_bootstrap_task,
+            'master_shell': schemas.task_v2_2_0.master_shell_task,
+            'erase_node': schemas.task_v2_2_0.erase_node_task,
+        }
+    }
 
-    schema = SchemaV5()
+    def validate(self, data_tree):
+        """See ValidatorBase documentation."""
+        report = super(ValidatorV5, self).validate(data_tree)
 
-    def __init__(self, *args, **kwargs):
-        super(ValidatorV5, self).__init__(*args, **kwargs)
-        self.bond_config_path = join_path(self.plugin_path, 'bond_config.yaml')
-        self.nic_config_path = join_path(self.plugin_path, 'nic_config.yaml')
-        self.node_config_path = join_path(self.plugin_path, 'node_config.yaml')
+        report.add_nodes(
+            checks.legacy_fuel_version(data_tree)
+        )
 
-    @property
-    def basic_version(self):
-        return '9.0'
+        report.add_nodes(
+            checks.mode_directive(data_tree)
+        )
 
-    def check_schemas(self):
-        super(ValidatorV5, self).check_schemas()
-        self.check_node_attributes_schema()
-        self.check_interface_attributes_schema(self.bond_config_path)
-        self.check_interface_attributes_schema(self.nic_config_path)
+        # check releases schema
+        for release in data_tree.get('releases', []):
+            release_report = utils.ReportNode('Checking release:')
+            for graph in release.get('graphs', []):
+                release_report.info('Graph: "{}"'.format(
+                    graph.get('type'))
+                )
+                release_report.add_nodes(
+                    checks.json_schema_is_valid(
+                        schema=schemas.graph_v9_1.graph,
+                        data=graph
+                    )
+                )
 
-    def check_node_attributes_schema(self):
-        self.validate_file_by_schema(self.schema.node_attributes_schema,
-                                     self.node_config_path,
-                                     allow_not_exists=True)
-
-    def check_interface_attributes_schema(self, file_path):
-        self.validate_file_by_schema(self.schema.node_nic_attributes_schema,
-                                     file_path,
-                                     allow_not_exists=True)
+            report.add_nodes(release_report)
+        return report
