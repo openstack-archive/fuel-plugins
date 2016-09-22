96ba62449266a1b9430f57b6b983f33e37dcd8fb
Elena Kosareva <ekosareva@mirantis.com>
2016-09-22 11:36:57 +0300

    Added new plugin version with node & nic attributes support
    * Added new plugin version supported by Fuel >= v9.0
    * Added validation for node/nic/bond plugin node's attributes
    
    Change-Id: I3e059940b910e67cca9b02a4d29179fa985414ff
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- fuel_plugin_builder/version_mapping.py
+++ fuel_plugin_builder/version_mapping.py
@@ -20,7 +20,7 @@ from fuel_plugin_builder import errors
 from fuel_plugin_builder import utils
 
 
-latest_version = '4.0.0'
+latest_version = '5.0.0'
 
 
 def get_mapping():
@@ -47,7 +47,15 @@ def get_mapping():
              'templates/v3/plugin_data/',
              'templates/v4/plugin_data/'],
          'validator': validators.ValidatorV4,
-         'builder': build.BuildPluginV4}]
+         'builder': build.BuildPluginV4},
+        {'version': '5.0.0',
+         'templates': [
+             'templates/base',
+             'templates/v3/plugin_data/',
+             'templates/v4/plugin_data/',
+             'templates/v5/plugin_data/'],
+         'validator': validators.ValidatorV5,
+         'builder': build.BuildPluginV5}]
 
 
 def get_plugin_for_version(version):
